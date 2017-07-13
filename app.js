7aeabf0a53b35a77e661f702b02928451a2a4167
janicewilson <janice.wilson@mac.com>
2017-07-13 06:05:33 -0700

    Set up git and Express.

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require( 'express' );
+const logger = require( 'morgan' );
+const bodyParser = require( 'body-parser' );
+
+const app = express();
+
+app.use(logger( 'dev' ));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.get('*', ( req, res ) => res.status( 200 ).send({
+     message: 'Online jean shopping that\'s not a pain in the ass. Jeanius.',
+}));
+
+module.exports = app;
