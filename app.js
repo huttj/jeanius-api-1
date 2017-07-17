035263543f8edef0a5fdb35bc1afa9bdbda835a0
janicewilson <janice.wilson@mac.com>
2017-07-17 13:47:05 -0700

    Added library to enable CORS.

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ const logger = require( 'morgan' );
 const bodyParser = require( 'body-parser' );
 const models = require( './server/models' );
 const fillmeup = require( 'fillmeup-sequelize');
+const cors = require('cors');
 
 const app = express();
+
+app.use(cors());
 
 app.use( logger( 'dev' ));
 
@@ -12,6 +15,11 @@ app.use( bodyParser.json());
 app.use( bodyParser.urlencoded({ extended: false }));
 
 require('./server/routes')(app);
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.get('*', ( req, res ) => res.status( 200 ).send({
      message: 'Online jean shopping that\'s not a pain in the ass. Jeanius.',
 }));
